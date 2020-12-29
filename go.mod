1b0b324970f282bd55881b36d7329ca173e78869
yaolx <634355316@qq.com>
2020-12-29 12:49:33 +0800

    add trace相关方法

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/yaolixiao/golang_common
 
 go 1.14
 
-require github.com/spf13/viper v1.7.1
+require (
+	github.com/spf13/viper v1.7.1
+	github.com/stretchr/testify v1.4.0 // indirect
+	golang.org/x/sys v0.0.0-20200116001909-b77594299b42 // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+)
--- main.go
+++ main.go
@@ -10,13 +10,18 @@ func main() {
 
 	// 测试资源初始化
 	if err := lib.Init("./conf/dev/"); err != nil {
-		fmt.Printf("main init fail. ~~~ err=%v\n", err)
+		fmt.Printf("main init fail. err=%v\n", err)
 		return
 	}
 
 	fmt.Println("init success.")
-	fmt.Println("ConfEnvPath=", lib.ConfEnvPath)
-	fmt.Println("ConfEnv=", lib.ConfEnv)
+	// fmt.Println("ConfEnvPath=", lib.ConfEnvPath)
+	// fmt.Println("addr=", lib.GetStringConf("base.http.addr"))
+	// fmt.Println("max_header_bytes=", lib.GetIntConf("base.http.max_header_bytes"))
+	// fmt.Println("write_timeout=", lib.GetIntConf("base.http.write_timeout"))
+	// fmt.Println("read_timeout=", lib.GetIntConf("base.http.read_timeout"))
+	// fmt.Println("lib.ConfBase.DebugMode=", lib.ConfBase.DebugMode)
+
 
 	// 测试net相关
 	// ips := lib.GetLocationIPs()
